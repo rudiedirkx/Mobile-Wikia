b789d330ab493c2b039433891cc42f214c473f1f
Rudie Dirkx <github@hotblocks.nl>
2018-11-03 17:00:26 +0100

    +theme color

--- tpl.header.php
+++ tpl.header.php
@@ -4,6 +4,7 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="theme-color" content="#333" />
 	<title><?= @$_title ? html($_title) . ' - ' : '' ?>Wikia</title>
 	<style><?= preg_replace('#\s*([\:\;\}\{\,])\s*#', '$1', file_get_contents(__DIR__ . '/style.css')) ?></style>
 </head>
